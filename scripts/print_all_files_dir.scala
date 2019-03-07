1b68f3bf65fc5531bebdd85ad787b867f617b005
haiaty <varottohaiaty@gmail.com>
2019-03-07 23:45:58 +0100

    - updates "print_all_files_dir"

--- scripts/print_all_files_dir.scala
+++ scripts/print_all_files_dir.scala
@@ -5,3 +5,10 @@ val filesHere = (new java.io.File(".")).listFiles
 
 for (file <- filesHere)
   println(file)
+
+// using filter on for
+
+print("#######")
+
+for (file <- filesHere if file.getName.endsWith(".php"))
+  println(file)
