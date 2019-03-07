40740012b16be9b6d782115c6c1e4e23ef2135bb
haiaty <varottohaiaty@gmail.com>
2019-03-07 22:47:19 +0100

    - adds "read_input_command_line_scala" code scripts

---
+++ scripts/read_input_command_line.scala
@@ -0,0 +1,11 @@
+/**
+  * ----------
+  *  Reading input from command line in a do while loop
+  * ----------
+  *
+  * */
+var line = ""
+do {
+  line = readLine()
+  println("Read: " + line)
+} while (line != "")
